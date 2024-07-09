bd84ecb10e69376e4bb5722770426291c016971f
Nethmi Hapuarachchi <140165153+nethmi02@users.noreply.github.com>
2024-07-09 08:20:12 +0530

    Update LabSheet4 -> Q1.scala

--- LabSheet4 -> Q1.scala
+++ LabSheet4 -> Q1.scala
@@ -23,7 +23,7 @@ object InventoryManagementSystem {
     def restockItem(itemName: String, quantity: Int): Unit = {
         val index = itemNames.indexOf(itemName)
         if (index != -1) {
-            itemQuantity(index) += quantity
+             itemQuantity(index) = itemQuantity(index) + quantity
             println(s"Restocked $itemName with $quantity units.")
         } else {
             println(s"Item $itemName does not exist.")
@@ -34,7 +34,7 @@ object InventoryManagementSystem {
         val index = itemNames.indexOf(itemName)
         if (index != -1) {
             if (itemQuantity(index) >= quantity) {
-                itemQuantity(index) -= quantity
+                itemQuantity(index) = itemQuantity(index) - quantity
                 println(s"Sold $quantity units of $itemName.")
             } else {
                 println(s"Not enough $itemName in stock to sell $quantity units.")
